a3c98baf71b114e11716a0c09c4737597f4b9f11
amcdowell <armcdowell@hotmail.com>
2013-02-25 15:29:11 +1300

    Fix search for LOT 47 Plan 10477
    Fixed in v2.1.4. This issue is caused because the search
    splits the search string into mulitple word parts using
    the space character has the word delimiter.
    
    To overcome this problem, I have updated the search to
    recognize and retain the POSIX escape character for space (i.e. \s).
    This will allow the user to explicitly indicate where a
    space should occur in the search string and match based
    on that. E.g. to search for LOT 47 in plan 10477 the
    search string to use is LOT\s47 10477
    
    [#89 state:fixed responsible:none milestone:v2.1.4]

--- ejbs/cadastre/src/main/java/org/sola/services/ejb/cadastre/businesslogic/CadastreEJB.java
+++ ejbs/cadastre/src/main/java/org/sola/services/ejb/cadastre/businesslogic/CadastreEJB.java
@@ -1,26 +1,30 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO). All rights
- * reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice,this list of conditions
- * and the following disclaimer. 2. Redistributions in binary form must reproduce the above
- * copyright notice,this list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution. 3. Neither the name of FAO nor the names of its
- * contributors may be used to endorse or promote products derived from this software without
- * specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
- * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 package org.sola.services.ejb.cadastre.businesslogic;
@@ -36,8 +40,9 @@ import org.sola.services.common.repository.CommonSqlProvider;
 import org.sola.services.ejb.cadastre.repository.entities.*;
 
 /**
- * EJB to manage data in the cadastre schema. Supports retrieving and saving of cadastre objects.
- * Also provides methods for retrieving reference codes from the administrative schema.
+ * EJB to manage data in the cadastre schema. Supports retrieving and saving of
+ * cadastre objects. Also provides methods for retrieving reference codes from
+ * the administrative schema.
  */
 @Stateless
 @EJB(name = "java:global/SOLA/CadastreEJBLocal", beanInterface = CadastreEJBLocal.class)
@@ -46,7 +51,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     /**
      * Retrieves all cadastre.cadastre_object_type code values.
      *
-     * @param languageCode The language code to use for localization of display values.
+     * @param languageCode The language code to use for localization of display
+     * values.
      */
     @Override
     public List<CadastreObjectType> getCadastreObjectTypes(String languageCode) {
@@ -74,9 +80,9 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Returns a maximum of 30 cadastre objects that have a name first part and/or name last part
-     * that matches the specified search string. This method supports partial matches and is case
-     * insensitive.
+     * Returns a maximum of 30 cadastre objects that have a name first part
+     * and/or name last part that matches the specified search string. This
+     * method supports partial matches and is case insensitive.
      *
      * @param searchString The search string to use
      * @return The list of cadastre objects matching the search string
@@ -86,10 +92,12 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
         Integer numberOfMaxRecordsReturned = 30;
         HashMap params = new HashMap();
         // Replace / and \ with space to improve the search
-        searchString = searchString.replaceAll("\\\\|\\/", " ");
+        if (!searchString.matches(".*\\\\s.*")) {
+            searchString = searchString.replaceAll("\\\\|\\/", " ");
+        }
         params.put("search_string", searchString);
         params.put(CommonSqlProvider.PARAM_LIMIT_PART, numberOfMaxRecordsReturned);
-        params.put(CommonSqlProvider.PARAM_ORDER_BY_PART, CadastreObject.QUERY_ORDER_BY_SEARCHBYPARTS); 
+        params.put(CommonSqlProvider.PARAM_ORDER_BY_PART, CadastreObject.QUERY_ORDER_BY_SEARCHBYPARTS);
         return getRepository().getEntityList(CadastreObject.class,
                 CadastreObject.QUERY_WHERE_SEARCHBYPARTS, params);
     }
@@ -108,20 +116,20 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
         HashMap params = new HashMap();
         params.put("search_string", searchString);
         params.put(CommonSqlProvider.PARAM_LIMIT_PART, numberOfMaxRecordsReturned);
-        params.put(CommonSqlProvider.PARAM_ORDER_BY_PART, CadastreObject.QUERY_ORDER_BY_SEARCHBYPARTS); 
+        params.put(CommonSqlProvider.PARAM_ORDER_BY_PART, CadastreObject.QUERY_ORDER_BY_SEARCHBYPARTS);
         return getRepository().getEntityList(CadastreObject.class,
                 CadastreObject.QUERY_WHERE_SEARCHBYALLPARTS, params);
     }
 
     /**
-     * Returns the cadastre object that is located at the point specified or null if there is no
-     * cadastre object at that location. Uses the PostGIS ST_Intersects function to perform the
-     * comparison.
+     * Returns the cadastre object that is located at the point specified or
+     * null if there is no cadastre object at that location. Uses the PostGIS
+     * ST_Intersects function to perform the comparison.
      *
      * @param x The x ordinate of the location
      * @param y The y ordinate of the location
-     * @param srid The SRID identifying the coordinate system for the x,y coordinate. Must match the
-     * SRID used by SOLA.
+     * @param srid The SRID identifying the coordinate system for the x,y
+     * coordinate. Must match the SRID used by SOLA.
      */
     @Override
     public CadastreObject getCadastreObjectByPoint(
@@ -136,8 +144,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Can be used to create a new cadastre object or save any updates to the details of an existing
-     * cadastre object.
+     * Can be used to create a new cadastre object or save any updates to the
+     * details of an existing cadastre object.
      *
      * @param cadastreObject The cadastre object to create/save.
      * @return The cadastre object after the save is completed.
@@ -161,7 +169,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves all cadastre objects linked to the specified Service through transaction.
+     * Retrieves all cadastre objects linked to the specified Service through
+     * transaction.
      *
      * @param serviceId Identifier of the Service
      */
@@ -174,14 +183,19 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Updates the status of cadastre objects associated with the specified transaction. The filter
-     * parameter can be used to indicate the where clause to apply.
+     * Updates the status of cadastre objects associated with the specified
+     * transaction. The filter parameter can be used to indicate the where
+     * clause to apply.
      *
-     * @param transactionId Identifier of the transaction associated to the cadastre objects to be
-     * updated
-     * @param filter The where clause to use when retrieving the cadastre objects. Must be {@linkplain CadastreObjectStatusChanger#QUERY_WHERE_SEARCHBYTRANSACTION_PENDING}
-     * or {@linkplain CadastreObjectStatusChanger#QUERY_WHERE_SEARCHBYTRANSACTION_TARGET}.
-     * @param statusCode The status code to set on the selected cadastre objects.
+     * @param transactionId Identifier of the transaction associated to the
+     * cadastre objects to be updated
+     * @param filter The where clause to use when retrieving the cadastre
+     * objects. Must be
+     * {@linkplain CadastreObjectStatusChanger#QUERY_WHERE_SEARCHBYTRANSACTION_PENDING}
+     * or
+     * {@linkplain CadastreObjectStatusChanger#QUERY_WHERE_SEARCHBYTRANSACTION_TARGET}.
+     * @param statusCode The status code to set on the selected cadastre
+     * objects.
      */
     @Override
     public void ChangeStatusOfCadastreObjects(
@@ -197,10 +211,11 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves the list of Cadastre Object Targets associated with the transaction. <p>Cadastre
-     * Object Targets are used to link the cadastre object to new transactions that may occur on the
-     * cadastre object after it has been initially created - for example the transaction to
-     * extinguish the cadastre object.</p>
+     * Retrieves the list of Cadastre Object Targets associated with the
+     * transaction. <p>Cadastre Object Targets are used to link the cadastre
+     * object to new transactions that may occur on the cadastre object after it
+     * has been initially created - for example the transaction to extinguish
+     * the cadastre object.</p>
      *
      * @param transactionId The identifier of the transaction
      */
@@ -246,19 +261,22 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves all node points from the underlying cadastre objects that intersect the specified
-     * bounding box coordinates. All of the node points within the bounding box are used to create a
-     * single geometry - {@linkplain CadastreObjectNode#geom}. The cadastre objects used as the
-     * source of the node points are also captured in the {@linkplain CadastreObjectNode#cadastreObjectList}.
+     * Retrieves all node points from the underlying cadastre objects that
+     * intersect the specified bounding box coordinates. All of the node points
+     * within the bounding box are used to create a single geometry -
+     * {@linkplain CadastreObjectNode#geom}. The cadastre objects used as the
+     * source of the node points are also captured in the
+     * {@linkplain CadastreObjectNode#cadastreObjectList}.
      *
      * @param xMin The xMin ordinate of the bounding box
      * @param yMin The yMin ordinate of the bounding box
      * @param xMax The xMax ordinate of the bounding box
      * @param yMax The yMax ordinate of the bounding box
-     * @param srid The SRID to use to create the bounding box. Must be the same SRID as the one used
-     * by the cadastre_object table.
-     * @return The CadastreObjectNode representing all node points within the bounding box as well
-     * as the list of cadastre objects used to obtain the node points.
+     * @param srid The SRID to use to create the bounding box. Must be the same
+     * SRID as the one used by the cadastre_object table.
+     * @return The CadastreObjectNode representing all node points within the
+     * bounding box as well as the list of cadastre objects used to obtain the
+     * node points.
      */
     @Override
     public CadastreObjectNode getCadastreObjectNode(
@@ -296,8 +314,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
      * @param yMin The yMin ordinate of the bounding box
      * @param xMax The xMax ordinate of the bounding box
      * @param yMax The yMax ordinate of the bounding box
-     * @param srid The SRID to use to create the bounding box. Must be the same SRID as the one used
-     * by the cadastre_object table.
+     * @param srid The SRID to use to create the bounding box. Must be the same
+     * SRID as the one used by the cadastre_object table.
      */
     @Override
     public CadastreObjectNode getCadastreObjectNodePotential(
@@ -326,9 +344,9 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves all Cadastre Object Node Targets associated to the transaction. <p>A Cadastre
-     * Object Node Target is used to identify the nodes that have been added, moved or removed as
-     * part of a redefinition transaction. </p>
+     * Retrieves all Cadastre Object Node Targets associated to the transaction.
+     * <p>A Cadastre Object Node Target is used to identify the nodes that have
+     * been added, moved or removed as part of a redefinition transaction. </p>
      *
      * @param transactionId The identifier of the transaction
      */
@@ -344,8 +362,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves the cadastre objects that have been associated with a cadastre redefinition
-     * transaction.
+     * Retrieves the cadastre objects that have been associated with a cadastre
+     * redefinition transaction.
      *
      * @param transactionId The identifier of the transaction
      */
@@ -361,7 +379,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Approves the changes to cadastre objects as a result of a cadastre redefinition.
+     * Approves the changes to cadastre objects as a result of a cadastre
+     * redefinition.
      *
      * @param transactionId The identifier of the transaction
      */
@@ -381,8 +400,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Process the list of spatialUnitChanges and apply the necessary changes to the spatial unit
-     * records.
+     * Process the list of spatialUnitChanges and apply the necessary changes to
+     * the spatial unit records.
      *
      * @param transactionId
      */
@@ -421,8 +440,8 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves the spatial unit change records that have been associated with a cadastre change or
-     * a cadastre redefinition transaction.
+     * Retrieves the spatial unit change records that have been associated with
+     * a cadastre change or a cadastre redefinition transaction.
      *
      * @param transactionId The identifier of the transaction
      */
@@ -448,10 +467,12 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves the Unit Parcel Group for the given spatial unit. Note that spatial unit group is
-     * used by SOLA Samoa to group parcels created for a Unit Plan Development.
+     * Retrieves the Unit Parcel Group for the given spatial unit. Note that
+     * spatial unit group is used by SOLA Samoa to group parcels created for a
+     * Unit Plan Development.
      *
-     * @param spatialUnitId The identifier of one of the spatial units that is part of the group.
+     * @param spatialUnitId The identifier of one of the spatial units that is
+     * part of the group.
      */
     @Override
     public UnitParcelGroup getUnitParcelGroupByParcelId(String spatialUnitId) {
@@ -466,8 +487,9 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Retrieves the Unit Parcel Group using the group name. Note that spatial unit group is
-     * used by SOLA Samoa to group parcels created for a Unit Plan Development.
+     * Retrieves the Unit Parcel Group using the group name. Note that spatial
+     * unit group is used by SOLA Samoa to group parcels created for a Unit Plan
+     * Development.
      *
      * @param groupName The name given to the Unit Parcel Group.
      */
@@ -484,8 +506,9 @@ public class CadastreEJB extends AbstractEJB implements CadastreEJBLocal {
     }
 
     /**
-     * Saves the Unit Parcel Group and any changes to the database. This method also does a bulk
-     * update to set the spatial unit level id for any new unit parcels that are created.
+     * Saves the Unit Parcel Group and any changes to the database. This method
+     * also does a bulk update to set the spatial unit level id for any new unit
+     * parcels that are created.
      *
      * @param group The UnitParcelGroup to save
      * @return The saved UnitParcelGroup
